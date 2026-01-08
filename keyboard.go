472a5632a88e279cf5c367907426268d7b482ace
karpovgv-gif <karpov.gv@mail.ru>
2026-01-08 12:42:00 +0300

    Add files via upload

--- keyboard.go
+++ keyboard.go
@@ -1,3 +1,4 @@
+// Этот пакет создан для считывания значений, введённых пользователем.
 package keyboard
 
 import (
@@ -7,6 +8,7 @@ import (
 	"strings"
 )
 
+// эта функция возвращает введённое пользователем число
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
